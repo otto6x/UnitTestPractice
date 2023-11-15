f88b8114d8b26bbf91ec56b2d30a736851c25bdd
otto6x <ethan16js@gmail.com>
2023-11-15 11:36:32 -0800

    Upload UnitTestPractice

--- Password.cpp
+++ Password.cpp
@@ -19,3 +19,59 @@ int Password::count_leading_characters(string phrase){
   }
   return repetition;
 }
+
+/*
+  returns whether the phrase has both at least one upper-case letter and
+  at least one lower-case letter
+*/
+bool Password::has_mixed_case(string phrase){
+  bool hasLower = false;
+  bool hasUpper = false;
+  
+  for(int i = 0; i < phrase.length(); i++){
+    if(islower(phrase[i]))
+      hasLower = true;
+    else if (isupper(phrase[i]))
+      hasUpper = true;
+  }
+
+  if(hasLower && hasUpper)
+    return true;
+  else 
+    return false;
+}
+
+/*
+  constructor sets the default password to "ChicoCA-95929"
+*/
+Password::Password(){
+  pass_history.push_back("ChicoCA-95929");
+}
+
+/*
+  receives a password and sets the latest in pass_history to it if and only
+  if it meets all criteria:
+    1. The password is at least 8 letters long
+    2. It has no more than 3 of the same leading characters
+    3. It has mixed case (at least one upper case and at least one lower case)
+    4. It was not a previous password in the history
+*/
+void Password::set(string phrase){
+  if(phrase.length() >= 8 && count_leading_characters(phrase) <= 3 && has_mixed_case(phrase) && phrase != pass_history.back()){
+    pass_history.push_back(phrase);
+  }
+}
+/*
+  receives a string and authenticates it against the latest password in the
+  pass_history, returning true for an exact match or false when it does not match
+  or if a password has not been set.
+*/
+bool Password::authenticate(string phrase){
+  if(pass_history.back() == "ChicoCA-95929"){
+    return false;
+  }else if(pass_history.back() == phrase){
+    return true;
+  }else{
+    return false;
+  }
+}
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -35,7 +35,7 @@ TEST(PasswordTest, first_character_repeats_later)
 	Password my_password;
 	ASSERT_EQ(1,my_password.count_leading_characters("abaaa"));
 }
-TEST(PasswordTest, must_fail)
+TEST(PasswordTest, must_fail_count)
 {
 	Password my_password;
 	ASSERT_EQ(1,my_password.count_leading_characters("aaa"));
@@ -50,4 +50,85 @@ TEST(PasswordTest, lower_upper)
 	Password my_password;
 	ASSERT_EQ(1,my_password.count_leading_characters("aA"));
 }
+TEST(PasswordTest, blank)
+{
+	Password my_password;
+	ASSERT_EQ(1,my_password.count_leading_characters(""));
+}
+TEST(PasswordTest, blank_space)
+{
+	Password my_password;
+	ASSERT_EQ(1,my_password.count_leading_characters(" "));
+}
+TEST(PasswordTest, blank_spaces)
+{
+	Password my_password;
+	ASSERT_EQ(1,my_password.count_leading_characters("   "));
+}
 
+TEST(PasswordTest, must_fail_lowercase)
+{
+	Password my_password;
+	ASSERT_EQ(true,my_password.has_mixed_case("aaa"));
+}
+TEST(PasswordTest, must_fail_uppercase)
+{
+	Password my_password;
+	ASSERT_EQ(true,my_password.has_mixed_case("AAA"));
+}
+TEST(PasswordTest, mixed_1_char_uppercase)
+{
+	Password my_password;
+	ASSERT_EQ(true,my_password.has_mixed_case("A"));
+}
+TEST(PasswordTest, mixed_1_char_lowercase)
+{
+	Password my_password;
+	ASSERT_EQ(true,my_password.has_mixed_case("a"));
+}
+TEST(PasswordTest, mixed_normal)
+{
+	Password my_password;
+	ASSERT_EQ(true,my_password.has_mixed_case("aBcdE"));
+}
+TEST(PasswordTest, mixed_special)
+{
+	Password my_password;
+	ASSERT_EQ(true,my_password.has_mixed_case(" !@#$%^&*():;,./<>?`~|-=_+"));
+}
+TEST(PasswordTest, mixed_special_with_normal)
+{
+	Password my_password;
+	ASSERT_EQ(true,my_password.has_mixed_case(" !@#$%^&*():;,./<>?`~|-=_+abcdABCD"));
+}
+
+TEST(PasswordTest, check_default)
+{
+	Password my_password;
+	ASSERT_EQ(false,my_password.authenticate("test"));
+}
+
+TEST(PasswordTest, add_new_working_password)
+{
+	Password my_password;
+	my_password.set("NewPassword");
+	ASSERT_EQ(true,my_password.authenticate("NewPassword"));
+}
+TEST(PasswordTest, add_new_short_password)
+{
+	Password my_password;
+	my_password.set("New");
+	ASSERT_EQ(false,my_password.authenticate("New"));
+}
+TEST(PasswordTest, add_new_repeat_password)
+{
+	Password my_password;
+	my_password.set("NNNNewPassword");
+	ASSERT_EQ(false,my_password.authenticate("NNNNewPassword"));
+}
+TEST(PasswordTest, add_new_fake_password)
+{
+	Password my_password;
+	my_password.set("NewPassword");
+	ASSERT_EQ(false,my_password.authenticate("Password"));
+}
